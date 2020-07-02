596f5fb3f342889c310fe305c46cd763b27e33c0
Robert Elliot <rob@lidalia.org.uk>
2020-07-02 17:40:50 +0100

    Handle patch & minor releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,21 @@
 
 set -euo pipefail
 
+checkoutNewMinorBranchFor() {
+  local releaseVersion=$1
+  local minorBranch
+  minorBranch=$(echo "$releaseVersion" | sed s/\\./_/g | sed s/0\$/x/)
+  git checkout -b "$minorBranch"
+}
+
+updateVersion() {
+  local gradleNextVersionCommand=$1
+  ./gradlew "$gradleNextVersionCommand"
+  local devVersion
+  devVersion=$(./gradlew -q version)
+  git commit -am "Prepared new development version $devVersion"
+}
+
 release() {
 
   local currentBranch
@@ -16,19 +31,15 @@ release() {
   git tag "$releaseVersion"
 
   if [[ $releaseVersion == *.0 ]]; then
-    local minorBranch
-    minorBranch=$(echo "$releaseVersion" | sed s/\\./_/g | sed s/0\$/x/)
-    git checkout -b "$minorBranch"
-    ./gradlew prepareNextPatchVersion
-    local devVersion
-    devVersion=$(./gradlew -q version)
-    git commit -am "Prepared new development version $devVersion"
+    # First release of this minor version
+    checkoutNewMinorBranchFor "$releaseVersion"
+    updateVersion prepareNextPatchVersion
+
+    git checkout "$currentBranch"
+    updateVersion prepareNextMinorVersion
   else
     git checkout "$currentBranch"
-    ./gradlew prepareNextMinorVersion
-    local devVersion
-    devVersion=$(./gradlew -q version)
-    git commit -am "Prepared new development version $devVersion"
+    updateVersion prepareNextPatchVersion
   fi
 }
 
